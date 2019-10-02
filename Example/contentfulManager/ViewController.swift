ed73a8a347421a383f4be91e9cb86ff9dc69b387
Perumal <dperumal@aligntech.com>
2019-10-02 19:29:37 +0530

    Initial commit

---
+++ Example/contentfulManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  contentfulManager
+//
+//  Created by Perumal1216 on 10/02/2019.
+//  Copyright (c) 2019 Perumal1216. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
